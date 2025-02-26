71d073e2a28749b75d3a4a312cbe77f4e0db5e4d
“David <00leedavid@gmail.com>
2025-02-25 23:21:16 -0600

    Updates to css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function myfunction() {
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let dropdown of dropdowns) {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
